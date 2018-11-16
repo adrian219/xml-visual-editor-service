3dfccc170cdad5462631b13a95c82577bd8855e3
wieczorek_a <adrian.wieczorek@tecna.pl>
2018-11-16 15:20:17 +0100

    init commit

---
+++ src/main/java/pl/adrianwieczorek/xmlvisualeditorservice/XmlVisualEditorServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.adrianwieczorek.xmlvisualeditorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlVisualEditorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlVisualEditorServiceApplication.class, args);
+	}
+}
