fa3780030b0b25df1101230b9aab18096cad4f14
wieczorek_a <adrian.wieczorek@tecna.pl>
2019-01-02 03:51:24 +0100

    clear code, remove necessary log

--- src/main/java/pl/adrianwieczorek/xmlvisualeditorservice/authentication/JwtAuthenticationFilter.java
+++ src/main/java/pl/adrianwieczorek/xmlvisualeditorservice/authentication/JwtAuthenticationFilter.java
@@ -58,7 +58,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         UsernamePasswordAuthenticationToken authentication = jwtTokenUtil.getAuthentication(authToken, SecurityContextHolder.getContext().getAuthentication(), userDetails);
         //UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
-        log.info("authenticated user " + username + ", setting security context");
         SecurityContextHolder.getContext().setAuthentication(authentication);
       }
     }
